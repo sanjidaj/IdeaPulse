b1629c79ea68e544cffce17b615a9fe0d479baa0
sanjidaj <sanjidajahan341@gmail.com>
2026-08-28 13:16:45 +0600

    Prepare backend for Vercel

--- server/server.js
+++ server/server.js
@@ -1,13 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
-// import mongoose from "mongoose";
 import cors from "cors";
 import authRoutes from "./routes/authRoutes.js";
 
 import connectDB from "./config/db.js";
 import ideaRoutes from "./routes/ideaRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
-import dashboardRoutes from "./routes/dashboardRoutes.js"
+import dashboardRoutes from "./routes/dashboardRoutes.js";
 import notificationRoutes from "./routes/notificationRoutes.js";
 
 dotenv.config();
@@ -19,10 +18,10 @@ const app = express();
 app.use(cors());
 
 app.use(express.json());
+
 app.use("/api/auth", authRoutes);
 app.use("/api/ideas", ideaRoutes);
-
-app.use("/api/users",userRoutes);
+app.use("/api/users", userRoutes);
 app.use("/api/dashboard", dashboardRoutes);
 app.use("/api/notifications", notificationRoutes);
 
@@ -30,10 +29,13 @@ app.get("/", (req, res) => {
   res.send("IdeaPulse API Running...");
 });
 
-
+export default app;
 // eslint-disable-next-line no-undef
-const PORT = process.env.PORT || 5000;
+if (process.env.NODE_ENV !== "production") {
+  // eslint-disable-next-line no-undef
+  const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`);
-});
+  app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`);
+  });
+}
